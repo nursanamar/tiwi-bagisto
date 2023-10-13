d2f32dbc596919a959089b90234052fa147f24b9
Nursan amar <nursan011@gmail.com>
2023-10-13 15:53:43 +0000

    fix package

--- packages/B2B/PriceQtyDisplay/src/Providers/PriceQtyDisplayServiceProvider.php
+++ packages/B2B/PriceQtyDisplay/src/Providers/PriceQtyDisplayServiceProvider.php
@@ -16,7 +16,6 @@ class PriceQtyDisplayServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
 
-        $this->loadRoutesFrom(__DIR__ . '/../Http/admin-routes.php');
 
         $this->loadRoutesFrom(__DIR__ . '/../Http/shop-routes.php');
 
@@ -50,9 +49,6 @@ class PriceQtyDisplayServiceProvider extends ServiceProvider
      */
     protected function registerConfig()
     {
-        $this->mergeConfigFrom(
-            dirname(__DIR__) . '/Config/admin-menu.php', 'menu.admin'
-        );
 
         $this->mergeConfigFrom(
             dirname(__DIR__) . '/Config/acl.php', 'acl'
