ec67b8d9adbb3752491761bdf650ef0f89e06124
Nursan amar <nursan011@gmail.com>
2024-04-22 00:04:48 +0800

    fix menu title

--- packages/B2B/AdminOrderCreation/src/Config/admin-menu.php
+++ packages/B2B/AdminOrderCreation/src/Config/admin-menu.php
@@ -3,7 +3,7 @@
 return [
     [
         'key' => 'adminordercreation',
-        'name' => 'AdminOrderCreation',
+        'name' => 'Buat Order Baru',
         'route' => 'admin.adminordercreation.index',
         'sort' => 2,
         'icon' => 'temp-icon',
